2c425858e7a3025b9cc07090cd6171bba17b5d3a
muhammed kayra <kayrabulut39@gmail.com>
2024-01-16 15:44:39 +0300

    versiyon 2 ekranda skorlar gözüküyor artık

--- model eğitimi/model_egit.py
+++ model eğitimi/model_egit.py
@@ -1,68 +1,82 @@
 import pandas as pd
+from sklearn.metrics import precision_score, recall_score, f1_score
 import numpy as np
-import torch
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
-from transformers import BertTokenizer, BertForSequenceClassification
-from transformers import TrainingArguments, Trainer
+import tensorflow as tf
+from transformers import TFBertForSequenceClassification, BertTokenizer
+from tensorflow.keras.callbacks import EarlyStopping
+
+# TensorFlow için uyumlu model ve tokenizer yükleme
+model_name = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract"
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = TFBertForSequenceClassification.from_pretrained(model_name, from_pt=True, num_labels=2)
+
 # Veri dosyasını oku
 data_filepath = '../ana_veri_seti/islenmis_diabetes_prediction_dataset.csv'
 df = pd.read_csv(data_filepath)
 
-# BiomedBERT modelini ve tokenizer'ını yükle
-model_name = 'microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext'
-tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertForSequenceClassification.from_pretrained(model_name, num_labels=2)
-
-
 X = list(df["text"])
 y = list(df["diabetes"])
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2,stratify=y)
-X_train_tokenized = tokenizer(X_train, padding=True, truncation=True, max_length=512)
-X_val_tokenized = tokenizer(X_val, padding=True, truncation=True, max_length=512)
 
-# Create torch dataset
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, encodings, labels=None):
-        self.encodings = encodings
-        self.labels = labels
+# İlk olarak veriyi %80 eğitim, %20 doğrulama/test olarak ayır
+X_train, X_val_test, y_train, y_val_test = train_test_split(X, y, test_size=0.2, stratify=y)
 
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        if self.labels:
-            item["labels"] = torch.tensor(self.labels[idx])
-        return item
+# Daha sonra kalan %20'yi yarıya bölerek %10 doğrulama ve %10 test seti oluştur
+X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, stratify=y_val_test)
 
-    def __len__(self):
-        return len(self.encodings["input_ids"])
-def compute_metrics(p):
-    print(type(p))
-    pred, labels = p
-    pred = np.argmax(pred, axis=1)
+# Verilerin tokenleştirilmesi
+X_train_tokenized = tokenizer(X_train, padding=True, truncation=True, max_length=512, return_tensors="tf")
+X_val_tokenized = tokenizer(X_val, padding=True, truncation=True, max_length=512, return_tensors="tf")
+# Test verilerinin tokenleştirilmesi
+X_test_tokenized = tokenizer(X_test, padding=True, truncation=True, max_length=512, return_tensors="tf")
 
-    accuracy = accuracy_score(y_true=labels, y_pred=pred)
-    recall = recall_score(y_true=labels, y_pred=pred, zero_division=0)
-    precision = precision_score(y_true=labels, y_pred=pred, zero_division=0)
-    f1 = f1_score(y_true=labels, y_pred=pred, zero_division=0)
+batch_size = 32
+# TensorFlow Dataset oluşturma
+test_dataset = tf.data.Dataset.from_tensor_slices((
+    dict(X_test_tokenized),
+    y_test
+)).batch(batch_size)
+# TensorFlow Dataset oluşturma
+train_dataset = tf.data.Dataset.from_tensor_slices((
+    dict(X_train_tokenized),
+    y_train
+)).shuffle(1000).batch(batch_size)
 
-    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
-train_dataset = Dataset(X_train_tokenized, y_train)
-val_dataset = Dataset(X_val_tokenized, y_val)
-# Define Trainer
-args = TrainingArguments(
-    output_dir="output",
-    num_train_epochs=1,
-    per_device_train_batch_size=8
-)
-trainer = Trainer(
-    model=model,
-    args=args,
-    train_dataset=train_dataset,
-    eval_dataset=val_dataset,
-    compute_metrics=compute_metrics
-)
-trainer.train()
-print(trainer.evaluate())
-trainer.save_model('CustomModel')
-# Tokenizer'ı kaydet
-tokenizer.save_pretrained('CustomModel')
+val_dataset = tf.data.Dataset.from_tensor_slices((
+    dict(X_val_tokenized),
+    y_val
+)).batch(batch_size)
+
+# EarlyStopping geri arama işlevini tanımlayın
+early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+
+# Modeli derleme
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=5e-5), 
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+              metrics=['accuracy'])
+
+# Modeli eğitme
+model.fit(train_dataset, epochs=30, validation_data=val_dataset, callbacks=[early_stopping])
+"""
+# Modelin test verileri üzerinde değerlendirilmesi
+# Modelin tahminlerini yapın
+y_pred = model.predict(test_dataset)
+# Tahminleri sınıf etiketlerine dönüştürün (örneğin, en yüksek olasılığa sahip sınıfı seçin)
+y_pred_labels = np.argmax(y_pred)
+# Gerçek sınıf etiketlerini alın
+y_true_labels = y_test
+# Precision (kesinlik) hesaplayın
+precision = precision_score(y_true_labels, y_pred_labels, average='weighted')
+
+# Recall (duyarlılık) hesaplayın
+recall = recall_score(y_true_labels, y_pred_labels, average='weighted')
+
+# F1 skoru hesaplayın
+f1 = f1_score(y_true_labels, y_pred_labels, average='weighted')
+
+# Sonuçları yazdırın
+print(f"Precision: {precision}, Recall: {recall}, F1 Score: {f1}")
+"""
+model.save_pretrained("CustomModel")
+tokenizer.save_pretrained("CustomModel")

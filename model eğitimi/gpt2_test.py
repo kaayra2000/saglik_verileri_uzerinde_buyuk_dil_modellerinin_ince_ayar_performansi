528f49b829b9fcd5e2dfdb77f9bba6e3ef2a4573
muhammed kayra <kayrabulut39@gmail.com>
2024-01-26 11:41:29 +0300

    gpt2 için test yazıldı

---
+++ model eğitimi/gpt2_test.py
@@ -0,0 +1,14 @@
+from transformers import pipeline, set_seed
+from sabitler import model_name
+generator = pipeline('text-generation', model=model_name)
+
+
+# while true ile sürekli girdi al
+while True:
+    # Girdi al
+    metin = input("Lütfen bir metin girin (çıkmak için 'exit' yazın): ")
+    # Çıkış koşulu
+    if metin.lower() == 'exit':
+        break
+    # Girdiyi modelden geçir
+    print(generator(metin, max_length=20, num_return_sequences=3))

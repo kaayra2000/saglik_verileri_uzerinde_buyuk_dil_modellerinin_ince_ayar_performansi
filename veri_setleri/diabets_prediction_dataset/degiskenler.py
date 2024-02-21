80011d4acf5dd2fced0da6c277fad4a062179030
muhammed kayra <kayrabulut39@gmail.com>
2024-02-21 09:06:25 +0300

    diyabet seçenekleri azaltıldı

--- veri_setleri/diabets_prediction_dataset/degiskenler.py
+++ veri_setleri/diabets_prediction_dataset/degiskenler.py
@@ -1,226 +1,126 @@
 secenekler_hba1c = {
     "Normal": [
         "Hastanın HbA1c seviyesi normal",
-        "Hastanın HbA1c değeri sağlıklı aralıkta",
-        "Hasta, normal glisemik kontrol altında",
-        "Hasta sağlıklı HbA1c aralığında",
-        "Hastanın HbA1c seviyesi uygun",
+        "Hastanın HbA1c değeri sağlıklı aralıkta"
     ],
     "Prediyabet": [
-        "Hasta prediyabet durumunda",
+        "Hasta HbA1c seviyesine göre prediyabet durumunda",
         "Hastanın HbA1c seviyesi prediyabetik",
-        "Hasta, hafif yükselmiş HbA1c seviyesine sahip",
-        "Hasta, diyabet öncesi durumda",
-        "Hastanın glisemik düzeyi prediyabetik",
     ],
     "Diyabet": [
         "Hasta, diyabetik HbA1c seviyesine sahip",
-        "Hastada diyabet saptanmıştır",
-        "Hastanın HbA1c değeri, diyabet riskini gösteriyor",
-        "Hasta için kritik HbA1c düzeyi",
-        "Hastanın HbA1c seviyesi ciddi şekilde yüksek",
+        "Hastada diyabet HbA1c seviyesi saptanmıştır"
     ],
 }
 
 secenekler_yas = {
     "çocuk": [
-        "Hasta çocuk yaşta",
         "Genç bir birey",
-        "Küçük yaş grubunda bir hasta",
         "Genç yaş grubundaki birey",
-        "Çocukluk çağında bir hasta",
     ],
     "yetişkin": [
         "Hasta yetişkin yaşta",
-        "Ergin bir birey",
         "Olgun yaş grubundaki hasta",
-        "Yetişkinlik dönemindeki birey",
-        "Aktif yaş grubundaki hasta",
     ],
     "yaşlı": [
         "Yaşlı bir hasta",
-        "İleri yaştaki birey",
-        "Yaşını almış bir hasta",
-        "Senior yaş grubundaki birey",
-        "Yaşlılık çağında bir hasta",
+        "İleri yaştaki birey"
     ],
 }
 
 secenekler_bmi = {
     "Düşük": [
         "Hastanın vücut kitle endeksi değeri düşük",
-        "Hastanın kilosu, vücut kitle endeksine göre düşük seviyede",
-        "Hasta, vücut kitle endeksine göre alt kilo sınırında",
-        "Hastada kilo eksikliği var",
-        "Hasta vücut kitle endeksine göre zayıf",
+        "Hastanın kilosu, vücut kitle endeksine göre düşük seviyede"
     ],
     "Normal": [
         "Hastanın vücut kitle endeksi değeri normal",
         "Hasta, sağlıklı vücut kitle endeksine sahip",
-        "Hastanın vücut kitle endeksi değerleri normal aralıkta",
-        "Hasta, ideal kiloda",
-        "Hastanın vücut kitle endeksi iyi",
     ],
     "Fazla Kilolu": [
         "Hasta fazla kilolu",
-        "Hasta vücut kitle endeksine göre aşırı şişman",
-        "Hasta, aşırı kilolu",
-        "Hastanın BMI değeri, normalin üstünde",
+        "Hasta vücut kitle endeksine göre aşırı şişman"
     ],
     "Obez": [
         "Hasta obezite durumunda",
-        "Hastanın BMI değeri çok yüksek",
         "Hasta obez",
-        "Hastada aşırı kilo problemi var",
-        "Hastanın obezite seviyesi ciddi",
+        "Hastada vücut kitle endeksine göre aşırı kilo problemi var"
     ],
 }
 
 secenekler_sigara_kullanimi = {
     "never": [
         "Hasta hiç sigara kullanmamıştır",
-        "Hasta, sigara ile hiç temas etmemiştir",
-        "Hastanın hiç sigara deneyimi yoktur",
         "Hasta, hayatı boyunca sigara içmemiştir",
-        "Hasta, sigarayla hiç ilişkisi olmamıştır",
     ],
     "No Info": [
         "Hasta hakkında sigara kullanımı bilgisi yoktur",
-        "Sigara kullanımı hakkında herhangi bir bilgi mevcut değildir",
-        "Hastanın sigara kullanım durumu bilinmemektedir",
-        "Hasta hakkında sigara geçmişi verisi yoktur",
-        "Hastanın sigara kullanımıyla ilgili net bilgi eksiktir",
+        "Sigara kullanımı hakkında herhangi bir bilgi mevcut değildir"
     ],
     "former": [
-        "Hasta eski bir sigara kullanıcısıdır",
         "Hasta, geçmişte sigara kullanmıştır",
-        "Hasta, daha önce düzenli olarak sigara içmiştir",
-        "Hasta, eskiden sigara içen biriydi",
-        "Hasta, geçmişte sigara bağımlısıydı",
+        "Hasta, daha önce düzenli olarak sigara içmiştir"
     ],
     "current": [
         "Hasta şu anda aktif olarak sigara kullanmaktadır",
-        "Hasta, günlük olarak sigara içmektedir",
-        "Hasta, halen sigara kullanıcısıdır",
-        "Hasta, düzenli olarak sigara içmektedir",
-        "Hasta, mevcut durumda sigara içmeye devam etmektedir",
+        "Hasta, halen sigara kullanıcısıdır"
     ],
     "not current": [
         "Hasta şu anda sigara kullanmamaktadır",
-        "Hasta, son zamanlarda sigarayı bırakmıştır",
-        "Hasta, şu an için sigara içmemektedir",
-        "Hasta, geçici bir süre için sigarayı bırakmıştır",
-        "Hasta, yakın zamanda sigarayı terk etmiştir",
+        "Hasta, son zamanlarda sigarayı bırakmıştır"
     ],
     "ever": [
         "Hasta, hayatının bir döneminde sigara kullanmıştır",
-        "Hasta, geçmişte belli bir süre sigara içmiştir",
-        "Hasta, daha önceleri sigara deneyimi yaşamıştır",
-        "Hasta, geçmişte ara sıra sigara içmiştir",
-        "Hasta, geçmişte sigara içen bir kişiydi",
+        "Hasta, geçmişte belli bir süre sigara içmiştir"
     ],
 }
 
 secenekler_kan_sekeri = {
     "Düşük": [
         "Kişinin kan şekeri düşük seviyededir.",
-        "Hastanın kan şekeri normalden düşük.",
-        "Kan şekeri düşüklüğü gözlemleniyor.",
-        "Düşük kan şekeri seviyeleri saptandı.",
-        "Kan şekeri seviyesi endişe verici derecede düşük.",
+        "Hastanın kan şekeri normalden düşük."
     ],
     "Normal": [
         "Kişinin kan şekeri seviyesi normal aralıktadır.",
-        "Kan şekeri değerleri sağlıklı görünüyor.",
-        "Hastanın kan şekeri dengeli ve normal.",
-        "Optimal kan şekeri seviyelerine sahip.",
-        "Kan şekeri seviyesi ideal durumda.",
+        "Kan şekeri değerleri sağlıklı görünüyor."
     ],
     "Yüksek Normal": [
         "Kişinin kan şekeri yüksek normal aralıkta.",
-        "Kan şekeri sınırda yüksek, dikkatli olunmalı.",
-        "Normalin üst sınırında kan şekeri seviyesi.",
-        "Yüksek normal kan şekeri tespit edildi.",
-        "Kan şekeri, normalin biraz üzerinde.",
+        "Kan şekeri sınırda yüksek."
     ],
     "Çok Yüksek": [
         "Kişinin kan şekeri aşırı yüksektir.",
-        "Hastanın kan şekeri tehlikeli derecede yüksek.",
-        "Diyabetik seviyelerde yüksek kan şekeri.",
-        "Kan şekeri seviyesi alarm verici derecede yüksek.",
-        "Ciddi yükseklikte kan şekeri saptandı.",
+        "Hastanın kan şekeri tehlikeli derecede yüksek."
     ],
 }
 
 hiper_tansiyon_var = [
     "Hasta hipertansiyon hastasıdır",
-    "Hastada hipertansiyon hastalığı tespit edilmiştir",
-    "Hastanın hipertansiyonu var",
-    "Hasta, hipertansiyon hastalığına sahiptir",
-    "Hastanın hipertansiyon hastası olduğu belirlenmiştir",
-    "Hastada hipertansiyon saptanmıştır",
-    "Hasta hipertansiyonla mücadele etmektedir",
-    "Hastanın yüksek tansiyon sorunu var",
-    "Hastanın kan basıncı yüksek bulunmuştur",
+    "Hastada hipertansiyon hastalığı tespit edilmiştir"
 ]
 
 hiper_tansiyon_yok = [
     "Hasta hipertansiyon hastası değildir",
-    "Hastada hipertansiyon hastalığı yoktur",
-    "Hasta, hipertansiyon açısından sağlıklıdır",
-    "Hastanın hipertansiyonu tespit edilmemiştir",
-    "Hasta hipertansiyon riski taşımamaktadır",
-    "Hastada herhangi bir hipertansiyon belirtisi görülmemiştir",
-    "Hastanın kan basıncı normal seviyededir",
-    "Hasta hipertansiyon açısından temiz bulunmuştur",
-    "Hastanın hipertansiyonla ilgili bir sorunu yoktur",
+    "Hastada hipertansiyon hastalığı yoktur"
 ]
 
 kalp_hastaligi_var = [
     "Hastada kalp hastalığı vardır",
-    "Hastanın kalp rahatsızlığı tespit edilmiştir",
-    "Hasta, kalp rahatsızlığına sahiptir",
-    "Hastanın kalp rahatsızlığı görülmüştür",
-    "Hasta kalp hastası olarak tanımlanmıştır",
-    "Tetkikler, hastanın kalp rahatsızlığı olduğunu göstermektedir",
-    "Hastada ciddi bir kalp rahatsızlığı saptanmıştır",
-    "Hastanın kalp hastası olduğu belirlenmiştir",
-    "Hastanın kalp durumu ciddi bir endişe kaynağıdır",
+    "Hastanın kalp rahatsızlığı tespit edilmiştir"
 ]
 
 kalp_hastaligi_yok = [
     "Hastada kalp hastalığı yoktur",
-    "Hastanın kalp rahatsızlığı bulunmamaktadır",
-    "Hasta, kalp açısından sağlıklıdır",
-    "Hastanın kalp rahatsızlığı tespit edilmemiştir",
-    "Hastanın kalp rahatsızlığı görülmemiştir",
-    "Tetkikler, hastanın kalp hastası olmadığını göstermektedir",
-    "Hastada herhangi bir kalp rahatsızlığı saptanmamıştır",
-    "Hastanın kalp sağlığı yerindedir",
-    "Hastanın kalp rahatsızlığı olmadığı anlaşılmıştır",
+    "Hastanın kalp rahatsızlığı bulunmamaktadır"
 ]
 
 
 diyabet_var = [
     "Hasta diyabet hastasıdır",
-    "Hastada diyabet hastalığı tespit edilmiştir",
-    "Hastanın diyabeti var",
-    "Hasta, diyabet hastalığına sahiptir",
-    "Tetkikler, hastanın diyabet hastası olduğunu göstermektedir",
-    "Hastada diyabet saptanmıştır",
-    "Hastanın diyabet hastası olduğu belirlenmiştir",
-    "Hastanın diyabetle mücadele ettiği anlaşılmaktadır",
-    "Hasta için diyabet tanısı konulmuştur",
+    "Hastada diyabet hastalığı tespit edilmiştir"
 ]
 
 diyabet_yok = [
     "Hasta diyabet hastası değildir",
-    "Hastada diyabet hastalığı yoktur",
-    "Hasta, diyabet açısından temiz bulunmuştur",
-    "Hastanın diyabeti bulunmamaktadır",
-    "Tetkikler, hastanın diyabet hastası olmadığını göstermektedir",
-    "Hastada diyabet saptanmamıştır",
-    "Hastanın diyabet hastası olmadığı belirlenmiştir",
-    "Hastanın diyabetle ilgili bir sorunu görülmemiştir",
-    "Hasta için diyabet tanısı konulmamıştır",
+    "Hastada diyabet hastalığı yoktur"
 ]

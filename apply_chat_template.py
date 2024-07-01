53b3ade3ce2e3c945b850c05fd2645cc2fa99213
Muhammed Kayra Bulut <98055130+kaayra2000@users.noreply.github.com>
2024-07-01 13:54:31 +0300

    chat template  fonksiyonu değişkenleri dışarıdan alıyor

--- apply_chat_template.py
+++ apply_chat_template.py
@@ -1,28 +1,51 @@
 import pandas as pd
 import argparse
+
+
 # CSV dosyasını parça parça okuyacak bir fonksiyon tanımlayın
 def process_csv_in_chunks(input_filepath, output_filepath, chunk_size=1000):
     # Çıktı dosyasını aç
-    with open(output_filepath, 'w', encoding='utf-8') as outfile:
-        outfile.write('text\n')
+    with open(output_filepath, "w", encoding="utf-8") as outfile:
+        outfile.write("text\n")
         # CSV dosyasını parça parça okuyun
         for chunk in pd.read_csv(input_filepath, chunksize=chunk_size):
             # Her parça için chat şablonu uygulayın
-            chunk['text'] = chunk.apply(apply_chat_template, axis=1)
+            chunk["text"] = chunk.apply(
+                lambda row: apply_chat_template(
+                    row["question_content"],
+                    row["question_answer"],
+                    row["doctor_title"],
+                    row["doctor_speciality"],
+                ),
+                axis=1,
+            )
             # Sonuçları yeni dosyaya yazın
-            chunk['text'].to_csv(outfile, index=False, header=False, mode='a')
+            chunk["text"].to_csv(outfile, index=False, header=False, mode="a")
 
-def apply_chat_template(row):
-    user_message = f"<|USER|> {row['question_content']}\n"
-    doctor_info = f"<|DOCTOR_TITLE|> {row['doctor_title']} <|SPECIALITY|> {row['doctor_speciality']}\n"
-    assistant_message = f"<|ASSISTANT|> {row['question_answer']} </s>\n"
+
+def apply_chat_template(
+    question_content,
+    question_answer,
+    doctor_title="bilinmiyor",
+    doctor_speciality="bilinmiyor",
+):
+    user_message = f"<|USER|> {question_content}\n"
+    doctor_info = (
+        f"<|DOCTOR_TITLE|> {doctor_title} <|SPECIALITY|> {doctor_speciality}\n"
+    )
+    assistant_message = f"<|ASSISTANT|> {question_answer} </s>\n"
     return f"{user_message}\n{doctor_info}\n{assistant_message}"
 
 
 if __name__ == "__main__":
     # Argümanları ayarlayın
     parser = argparse.ArgumentParser(description="CSV dosyasını parça parça işleyin.")
-    parser.add_argument('input_filepath', type=str, help='Giriş CSV dosyasının yolu', default="sohbetler20000.csv")
+    parser.add_argument(
+        "input_filepath",
+        type=str,
+        help="Giriş CSV dosyasının yolu",
+        default="sohbetler20000.csv",
+    )
 
     # Argümanları alın
     args = parser.parse_args()

1fd934b54215f427747612605bfe3c9e71008e80
kayra <kayrabulut39@gmail.com>
2024-07-07 00:37:09 +0300

    batch dosyası oluşturma ilk hali verildi

---
+++ çeviri/doktorsitesi/batched/batch_dosyasi_olustur.py
@@ -0,0 +1,55 @@
+import csv
+import sys
+import os
+import json
+
+sys.path.append("../..")
+from fonksiyonlar import veri_yolu_al
+
+def create_batch_request(row, index):
+    return {
+        "custom_id": f"request-{index}",
+        "method": "POST",
+        "url": "/v1/chat/completions",
+        "body": {
+            "model": "ft:gpt-3.5-turbo-0125:kayra::9i7HydFK",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": "Kullanıcının yazdığı metni temizle ve gereksiz bilgileri kaldır. Sadece gerekli olan ve anlamlı bilgileri bırak. Bu süreçte aşağıdaki kurallara uy:\nAdres cümlelerini, anlamı bozmadan direkt çıkart. Adrese atıf yapma.\nLinkleri doğrudan çıkart, anlamı bozmadan linklere atıf yapma.\nÜnvan ve isimleri doğrudan çıkart. Örneğin, 'Prof. Dr. Ahmet' yerine 'Dr.' ya da 'doktor' yeterli.\nYanlış yazılan kelimeleri düzelt. Örneğin, 'Yadrım edin' yerine 'Yardım edin'.\nTarihleri, anlam bütünlüğünü bozmayacak şekilde tamamen kaldır.\nBağlam bağımlı cümleleri, anlam bütünlüğünü bozmayacak şekilde tamamen kaldır. Örneğin, 'geçen gün sizinle görüşmüştük' gibi cümleleri çıkar.\nBu bir doktor-hasta verisi olduğu için, soru cevabı içeren veri seti temizlemesinde kullanılacak ve sonrasında bir chatbot eğitmede kullanılacak. Bu bilgiye göre temizleme yap.",
+                },
+                {
+                    "role": "user",
+                    "content": row["question_content"],
+                }
+            ],
+            "max_tokens": 4096
+        }
+    }
+
+def save_batch_requests(data, batch_file_path, start_index):
+    with open(batch_file_path, "w", encoding="utf-8") as batch_file:
+        for index, row in enumerate(data[start_index:], start=start_index):
+            request = create_batch_request(row, index)
+            batch_file.write(json.dumps(request, ensure_ascii=False) + "\n")
+
+def cevir_kaydet_jsonl(veri_yolu):
+    batch_file_path = veri_yolu.replace(".csv", "_batch.jsonl")
+    cleaned_file_path = veri_yolu.replace(".csv", "_cleaned.csv")
+    # Girdi dosyasını oku
+    with open(veri_yolu, "r", newline="", encoding="utf-8") as infile:
+        reader = csv.DictReader(infile)
+        data = list(reader)
+
+    # Mevcut çevrilmiş dosyanın var olup olmadığını kontrol edin
+    start_index = 0
+    if os.path.exists(cleaned_file_path):
+        with open(cleaned_file_path, "r", newline="", encoding="utf-8") as outfile:
+            reader = csv.DictReader(outfile)
+            existing_data = list(reader)
+            start_index = len(existing_data)
+
+    save_batch_requests(data, batch_file_path, start_index)
+
+veri_yolu = veri_yolu_al()
+cevir_kaydet_jsonl(veri_yolu)

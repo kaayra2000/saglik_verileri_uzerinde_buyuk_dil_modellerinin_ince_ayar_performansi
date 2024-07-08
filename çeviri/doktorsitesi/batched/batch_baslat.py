1dd1cae76964289866fd447bcc45ce907a810bb7
Muhammed Kayra Bulut <98055130+kaayra2000@users.noreply.github.com>
2024-07-08 09:28:29 +0300

    batch başlatma artık dosyadan okuyarak

--- çeviri/doktorsitesi/batched/batch_baslat.py
+++ çeviri/doktorsitesi/batched/batch_baslat.py
@@ -1,21 +1,65 @@
-import sys
-
+import time
 from openai import OpenAI
 
+
+def get_file_id(file_line):
+    # FileObject(id='file-EWOCP7WaRmJVUmdJhibV4tcf', bytes=1570909, created_at=1720417181, filename='sohbetler20000_batch_1310-1809.jsonl', object='file', purpose='batch', status='processed', status_details=None)
+    parts = file_line.split(",")
+    for part in parts:
+        if "id=" in part:
+            return part.split("'")[1]
+    return None
+
+
+# OpenAI API anahtarını oku
 with open("../../../api_key.txt", "r") as file:
     api_key = file.read().strip()
+
 client = OpenAI(api_key=api_key)
 
-batch_input_file_id = "file-LGnKPscHXqLSJ8mFH0YfkBMW"
+# batch_file_info.txt dosyasını oku
+with open("batch_file_info.txt", "r") as file:
+    file_lines = file.readlines()
 
-sonuc = client.batches.create(
-    input_file_id=batch_input_file_id,
-    endpoint="/v1/chat/completions",
-    completion_window="24h",
-    metadata={
-      "description": "20k doktorsitesi"
-    }
-)
+for line in file_lines:
+    batch_input_file_id = get_file_id(line.strip())
+    if batch_input_file_id:
+        # Yeni batch başlat
+        sonuc = client.batches.create(
+            input_file_id=batch_input_file_id,
+            endpoint="/v1/chat/completions",
+            completion_window="24h",
+            metadata={"description": "20k doktorsitesi"},
+        )
 
-with open("batch_info.txt", "w") as file:
-    file.write(str(sonuc))
+        batch_id = sonuc.id
+        print(f"Batch {batch_id} başlatıldı.")
+        # Batch durumunu kontrol et
+        while True:
+            batch_status = client.batches.retrieve(batch_id=batch_id)
+            status = batch_status.status
+
+            if status == "completed":
+                print(f"Batch {batch_id} başarıyla tamamlandı.")
+                break
+            elif status == "failed":
+                print(f"Batch {batch_id} başarısız oldu.")
+                exit(1)  # İşlemi sonlandır
+            elif status == "in_progress":
+                print(f"Batch {batch_id} devam ediyor.")
+            elif status == "finalizing":
+                print(f"Batch {batch_id} zaman aşımına uğradı.")
+            elif status == "validating":
+                print(f"Batch {batch_id} doğrulanıyor.")
+            elif status == "cancelling":
+                print(f"Batch {batch_id} iptal ediliyor.")
+            elif status == "cancelled":
+                print(f"Batch {batch_id} iptal edildi.")
+                exit(1)  # İşlemi sonlandır
+            time.sleep(60)  # 60 saniye bekle ve tekrar kontrol et
+
+        # Batch bilgilerini yaz
+        with open("batch_info.txt", "a") as file:
+            file.write(str(sonuc) + "\n")
+
+print("Tüm batch işlemleri tamamlandı.")
--- çeviri/doktorsitesi/batched/batch_sonucu_al.py
+++ çeviri/doktorsitesi/batched/batch_sonucu_al.py
@@ -1,5 +1,11 @@
-from openai import OpenAI
 import json
+import os
+import sys
+
+sys.path.append("../..")
+from fonksiyonlar import veri_yolu_al
+from openai import OpenAI
+
 # API anahtarını oku
 with open("../../../api_key.txt", "r", encoding="utf-8") as file:
     api_key = file.read().strip()
@@ -7,19 +13,46 @@ with open("../../../api_key.txt", "r", encoding="utf-8") as file:
 # OpenAI istemcisini oluştur
 client = OpenAI(api_key=api_key)
 
-# Dosya içeriğini al
-file_id = "file-uXh7ifyBvmEI41KBDinv9Klg"
-response = client.files.content(file_id)
+# batch_info.txt dosyasını oku
+with open("batch_info.txt", "r", encoding="utf-8") as file:
+    lines = file.readlines()
 
-# İçeriği UTF-8 olarak dekode et ve satır satır işle
-content = response.content.decode("utf-8")
-data = {}
-data["data"] = []
-for line in content.splitlines():
-        json_obj = json.loads(line)
-        data["data"].append(json_obj)
+# Sonuçların kaydedileceği klasör
+output_folder = "sonuclar"
+output_prefix = veri_yolu_al()
+os.makedirs(output_folder, exist_ok=True)
 
+# Batch ID'lerini içeren satırları işle
+for line in lines:
+    line = line.strip()
+    if line:
+        file_id = line.split("'")[1]
 
-# JSONL içeriğini UTF-8 olarak kaydet
-with open("output.json", "w", encoding="utf-8") as outfile:
-    json.dump(data, outfile, ensure_ascii=False, indent=4)
+        # Batch içeriğini al
+        response = client.files.content(file_id)
+        content = response.content.decode("utf-8")
+
+        # İçeriği JSON objelerine ayır
+        data = {"data": []}
+        for line in content.splitlines():
+            json_obj = json.loads(line)
+            data["data"].append(json_obj)
+
+        # İlk ve son elemanın custom_id'sini al
+        first_custom_id = data["data"][0]["custom_id"]
+        last_custom_id = data["data"][-1]["custom_id"]
+
+        # Dosya adını oluştur
+        start_id = first_custom_id.split("-")[1]
+        end_id = last_custom_id.split("-")[1]
+        output_postfix = f"{start_id}-{end_id}_output.json"
+        output_file_name = f"{output_prefix}_{output_postfix}"
+        # Sonuçları dosyaya yaz
+        with open(
+            os.path.join(output_folder, output_file_name), "w", encoding="utf-8"
+        ) as outfile:
+            json.dump(data, outfile, ensure_ascii=False, indent=4)
+
+        print(f"{output_file_name} dosyası oluşturuldu.")
+
+print("Tüm batch işlemleri tamamlandı.")

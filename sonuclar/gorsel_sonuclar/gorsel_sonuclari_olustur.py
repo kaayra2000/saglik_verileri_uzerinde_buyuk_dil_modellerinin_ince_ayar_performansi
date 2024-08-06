08434e2eed0b8d94ebc8ee982bf312c6dacb75b5
Muhammed Kayra Bulut <98055130+kaayra2000@users.noreply.github.com>
2024-08-06 12:58:11 +0300

    winpct ve elo çizdirildi

--- sonuclar/gorsel_sonuclar/gorsel_sonuclari_olustur.py
+++ sonuclar/gorsel_sonuclar/gorsel_sonuclari_olustur.py
@@ -160,8 +160,8 @@ bert_specific_file_path = "bert_specific_chart.svg"
 bert_spesific_model_file_path = "bert_specific_model_chart.svg"
 bert_specific_matrix_model = []
 
-for content in json_contents:
-    for model_name in model_names:
+for model_name in model_names:
+    for content in json_contents:
         if model_name in content:
             model_content = content[model_name]
             bert_specific_matrix_model.append(
@@ -278,8 +278,8 @@ bleu_specific_file_path = "bleu_specific_chart.svg"
 bleu_spesific_model_file_path = "bleu_specific_model_chart.svg"
 bleu_specific_matrix_model = []
 
-for content in json_contents:
-    for model_name in model_names:
+for model_name in model_names:
+    for content in json_contents:
         if model_name in content:
             model_content = content[model_name]
             bleu_specific_matrix_model.append(model_content["bleu"]["precisions"])
@@ -392,9 +392,8 @@ rouge_general_matrix = []
 rouge_specific_file_path = "rouge_specific_chart.svg"
 rouge_spesific_model_file_path = "rouge_specific_model_chart.svg"
 rouge_specific_matrix_model = []
-
-for content in json_contents:
-    for model_name in model_names:
+for model_name in model_names:
+    for content in json_contents:
         if model_name in content:
             model_content = content[model_name]
             rouge_specific_matrix_model.append(
@@ -510,8 +509,8 @@ meteor_general_file_path = "meteor_general_chart.svg"
 meteor_general_matrix = []
 
 
-for content in json_contents:
-    for model_name in model_names:
+for model_name in model_names:
+    for content in json_contents:
         if model_name in content:
             model_content = content[model_name]
             meteor_general_matrix.append([model_content["meteor"]["meteor"]])
@@ -556,8 +555,8 @@ cer_wer_general_file_path = "cer_wer_general_chart.svg"
 cer_wer_general_matrix = []
 
 
-for content in json_contents:
-    for model_name in model_names:
+for model_name in model_names:
+    for content in json_contents:
         if model_name in content:
             model_content = content[model_name]
             cer_wer_general_matrix.append([model_content["cer"], model_content["wer"]])
@@ -577,6 +576,97 @@ plot_custom_bar_chart(
     bar_width=0.4,
     show_values=True,
     file_path=cer_wer_general_file_path,
+    fig_size=(16, 11),
+    x_label_fontsize=15,
+    x_label_rotation=25,
+    y_label_fontsize=25,
+    title_fontsize=28,
+    x_title_fontsize=25,
+    subplots_adjust_bottom=0.25,
+    subplots_adjust_top=0.95,
+    subplots_adjust_left=0.08,
+    subplots_adjust_right=0.9,
+    value_fontsize=15,
+    y_tick_label_fontsize=15,
+    float_len=2,
+)
+
+"""
+ELO KISMI
+"""
+
+elo_wer_general_label = ["ELO Skoru"]
+elo_wer_general_file_path = "elo_general_chart.svg"
+elo_wer_general_matrix = []
+
+
+for model_name in model_names:
+    for content in json_contents:
+        if model_name in content:
+            model_content = content[model_name]
+            elo_wer_general_matrix.append([model_content["ELO"]])
+            break
+
+
+plot_custom_bar_chart(
+    elo_wer_general_matrix,
+    model_names,
+    colors,
+    elo_wer_general_label,
+    show_data_lables=True,
+    title="Farklı Modellerin ELO Skor Performans Değerleri",
+    x_axis_label="Model",
+    y_axis_label="ELO Skor Değerleri",
+    legend_location="upper left",
+    bar_width=0.4,
+    show_values=True,
+    file_path=elo_wer_general_file_path,
+    fig_size=(16, 11),
+    x_label_fontsize=15,
+    x_label_rotation=25,
+    y_label_fontsize=25,
+    title_fontsize=28,
+    x_title_fontsize=25,
+    subplots_adjust_bottom=0.25,
+    subplots_adjust_top=0.95,
+    subplots_adjust_left=0.08,
+    subplots_adjust_right=0.9,
+    value_fontsize=15,
+    y_tick_label_fontsize=15,
+    float_len=2,
+)
+
+
+"""
+WinPct KISMI
+"""
+
+winpct_wer_general_label = ["WinPct Skoru"]
+winpct_wer_general_file_path = "winpct_general_chart.svg"
+winpct_wer_general_matrix = []
+
+
+for model_name in model_names:
+    for content in json_contents:
+        if model_name in content:
+            model_content = content[model_name]
+            winpct_wer_general_matrix.append([model_content["WinPct"]])
+            break
+
+
+plot_custom_bar_chart(
+    winpct_wer_general_matrix,
+    model_names,
+    colors,
+    winpct_wer_general_label,
+    show_data_lables=True,
+    title="Farklı Modellerin WinPct Skor Performans Değerleri",
+    x_axis_label="Model",
+    y_axis_label="WinPct Skor Değerleri",
+    legend_location="upper left",
+    bar_width=0.4,
+    show_values=True,
+    file_path=winpct_wer_general_file_path,
     fig_size=(16, 11),
     x_label_fontsize=15,
     x_label_rotation=25,
